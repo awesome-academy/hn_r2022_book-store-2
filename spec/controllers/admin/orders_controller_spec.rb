c7d76022767cc20a92935dacb7c134caef51a530
ninhnv-1216 <nguyen.van.ninh@sun-asterisk.com>
2022-03-14 22:21:08 +0700

    Rspec model controller for Order

---
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe Admin::OrdersController, type: :controller do
+  let(:order){FactoryBot.create :order}
+
+  describe "before action callbacks" do
+    it {is_expected.to use_before_action(:load_newest_order)}
+  end
+
+  describe "PATCH #update" do
+    context "when update order status success" do
+      before do
+        patch :update, params: {id: order.id, order:{status: "pending"}}
+      end
+      it "should status is accepted" do
+        order.status_accepted!
+        expect(order.status).to eq "accepted"
+      end
+    end
+    context "when update order status fail" do
+      before do
+        allow_any_instance_of(Order).to receive(:status_accepted!).and_raise(ActiveRecord::RecordInvalid)
+        patch :update, params: {id: order.id, order:{status: "accepted"}}
+      end
+      it "should flash danger" do
+        expect(flash[:danger]).to eq I18n.t "update_fail"
+      end
+      it "should redirect to admin orders path" do
+        expect(response).to redirect_to admin_orders_path
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "when reject order success" do
+      before do
+        delete :destroy, params: {id: order.id}
+      end
+      it "should status is rejected" do
+        order.status_rejected!
+        expect(order.status_rejected!).to eq true
+      end
+    end
+    context "when reject order fail" do
+      before do
+        allow_any_instance_of(Order).to receive(:status_rejected!).and_raise(ActiveRecord::RecordInvalid)
+        delete :destroy, params: {id: order.id, order:{status: "rejected"}}
+      end
+      it "should flash danger" do
+        expect(flash[:danger]).to eq I18n.t "delete_fail"
+      end
+      it "should redirect to admin orders path" do
+        expect(response).to redirect_to admin_orders_path
+      end
+    end
+  end
+end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe "Associations" do
+    it{is_expected.to belong_to(:user)}
+    it{is_expected.to have_many(:order_details).dependent(:destroy)}
+    it{is_expected.to have_many(:books).through(:order_details)}
+  end
+
+  describe "Enums" do
+    it {
+      should define_enum_for(:status)
+        .with_values(pending: 0, rejected: 1, accepted: 2,
+                     shipped: 3, delivered: 4, canceled: 5)
+        .with_prefix
+    }
+  end
+
+  describe "Validations" do
+    before {FactoryBot.build :order}
+
+    context "with delivery address field" do
+      it{is_expected.to validate_presence_of(:delivery_address)}
+    end
+
+    context "with delivery phone field" do
+      it{is_expected.to validate_presence_of(:delivery_phone)}
+      it{is_expected.to validate_length_of(:delivery_phone).is_at_most(Settings.digit_10)}
+    end
+  end
+
+  describe "private methods" do
+    let!(:order){FactoryBot.create :order}
+
+    context "when order status change" do
+      it "restore book quantity" do
+        quantity = order.books.first.quantity
+        order.status_rejected!
+        order.reload
+        expect(order.books.first.quantity).to eq(quantity + order.order_details.first.quantity)
+      end
+      it "reduce book quantity" do
+        quantity = order.books.first.quantity
+        order.status_pending!
+        order.reload
+        expect(order.books.first.quantity).to eq(quantity - order.order_details.first.quantity)
+      end
+    end
+  end
+end
