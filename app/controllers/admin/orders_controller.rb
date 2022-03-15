b39976eceea67f449bf1fc1a905e8dc68791751e
ninhnv-1216 <nguyen.van.ninh@sun-asterisk.com>
2022-03-15 17:31:33 +0700

    Rspec model controller for Order
    This reverts commit bcca8e80cf0e999f1a6ec8b9fb72f194ecf0326f.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,7 @@ class Admin::OrdersController < AdminController
 
   def update
     ActiveRecord::Base.transaction do
-      @order.public_send("status_#{params[:order][:status]}!")
+    @order.public_send("status_#{params[:order][:status]}!")
     end
     refresh
   rescue ActiveRecord::RecordInvalid
